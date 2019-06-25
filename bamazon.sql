c1a830b9f0534bebcc6cf8115628c5c66117812c
Trevor Fairbank <Trevorfairbank92@gmail.com>
2019-06-24 21:24:12 -0700

    fixed buyProduct function to update table and created a showTable function

--- bamazon.sql
+++ bamazon.sql
@@ -6,39 +6,39 @@ CREATE TABLE products (
 item_id INTEGER AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(255) NOT NULL,
 department_name VARCHAR(50) NOT NULL,
-price INTEGER,
+price DECIMAL(4,2),
 stock_quantity INTEGER,
 PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("4 pack drain snake cleaning tool", "maintenance", 8.99,20);
+VALUES("4 pack drain snake cleaning tool", "maintenance", 9,20);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("Great Stuff pestblock", "maintenance", 6.74,50);
+VALUES("Great Stuff pestblock", "maintenance", 7,50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("Golf Training Mat", "sport", 30.59,10);
+VALUES("Golf Training Mat", "sport", 31,10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("Putt-A-Bout Putting Mat", "sport", 18.56,18);
+VALUES("Putt-A-Bout Putting Mat", "sport", 19,18);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("Golf Chipping Net", "sport", 29.99,40);
+VALUES("Golf Chipping Net", "sport", 30,40);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("Farkle", "board game", 7.80,100);
+VALUES("Farkle", "board game", 8,100);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("TetherPro USB 3.0 Super-speed micro-B cable, 15 feet", "camera accessories", 48.95,80);
+VALUES("TetherPro USB 3.0 Super-speed micro-B cable, 15 feet", "camera accessories", 49,80);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("Internet's best pet gate with arched top | 3 panel", "pet gate", 229.94,30);
+VALUES("Internet's best pet gate with arched top | 3 panel", "pet gate", 35,30);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("LaCie Rugged RAID Thunderbolt & USB HARD DRIVE 4TB", "computer accessories", 48.95,80);
+VALUES("LaCie Rugged RAID Thunderbolt & USB HARD DRIVE 4TB", "computer accessories", 230,80);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("From the Ground Up: A Journey to Reimagine the Promise of America", "books", 19.60,200);
+VALUES("From the Ground Up: A Journey to Reimagine the Promise of America", "books", 20,200);
 
-SELECT*FROM products WHERE item_id(1);
+SELECT*FROM products WHERE stock_quantity;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -14,34 +16,77 @@ connection.connect(err => {
     if (err) throw (err);
     console.log("You are connected as id " + connection.threadId);
     connection.query("SELECT*FROM products", function (err, response) {
-        if (err) throw (err);
+        if (err) throw err;
         console.table(response);
         buyProduct();
     })
 })
 
 function buyProduct() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is the item_ID of the product you would like to buy?",
-                name: "item_id"
-            },
-            {
-                type: "input",
-                message: "How many units would you like to buy?",
-                name: "quantity"
-            }
-        ])
-        .then(function(response) {
-            console.log(response);
+
+    connection.query("SELECT*FROM products", function (err, response) {
+
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < response.length; i++) {
+                            choiceArray.push(response[i].product_name + " Price: $ " + response[i].price);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What item would you like to buy?"
+                },
+                {
+                    type: "input",
+                    message: "How many units would you like to buy?",
+                    name: "quantity"
+                }
+            ])
+            .then(function (data) {
+                //console.log(data);
+
+                //set selected item row into a variable
+                let itemChosen;
+
+                for (var i = 0; i < response.length; i++) {
+                    if ((response[i].product_name + " Price: $ " + response[i].price) === data.choice) {
+                        itemChosen = response[i];
+                    }
+                }
+
+                if (parseInt(itemChosen.stock_quantity) > parseInt(data.quantity)) {
+
+                    console.log(itemChosen);
+
+                    const newStock = parseInt(itemChosen.stock_quantity) - parseInt(data.quantity);
+                    connection.query(
+                        "UPDATE products SET stock_quantity= ? WHERE item_id= ?",
+                        [newStock, itemChosen.item_id],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("You have bought " + data.quantity + " " + itemChosen.product_name);
+                            showTable();
+                            buyProduct();
+                        }
+                    );
+                }
+
+                else {
+                    console.log("Sorry, not enough in our inventory.");
+                }
 
 
-        });
+            })
+    })
 }
 
-function quantityCheck(){
-    
-
+function showTable() {
+    connection.query("SELECT*FROM products", function (err, response) {
+        if(err) throw err;
+        console.table(response);
+    })
 }
